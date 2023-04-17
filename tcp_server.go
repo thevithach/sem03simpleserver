115233184a5837e5733027991898a53b11e45d78
thevithach <vitt@uia.no>
2023-04-17 09:57:59 +0000

    boseclat2

--- tcp_server.go
+++ tcp_server.go
@@ -53,4 +53,7 @@ func main() {
 		}
 	}()
 	wg.Wait()
+	dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n]))), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
+	log.Println("Dekrypter melding: ", string(dekryptertMelding))
+	switch msg := string(dekrypterMelding) { ..
 }
